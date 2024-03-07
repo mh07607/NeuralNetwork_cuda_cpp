c66b646df74ffe5faab0a23f2235966d88c6dec5
Arsal <mh07607@st.habib.edu.pk>
2024-03-07 08:35:58 +0500

    now to fix kernel output

--- gpu2/NeuralNetwork.h
+++ gpu2/NeuralNetwork.h
@@ -54,9 +54,9 @@ public:
 
 	Eigen::MatrixXf forwardPropagation(Eigen::MatrixXf& input)
 	{
-		// this->input = input;  
-		// this->output = input * weights + bias;  
-		// std::cout << "Actual output: " << this->output << std::endl;
+		this->input = input;  
+		this->output = input * weights + bias;  
+		std::cout << "Actual output: " << this->output << std::endl;
 
 		this->input = input;
 		float * output_arr;
@@ -70,7 +70,7 @@ public:
 		cudaDeviceSynchronize();
 		cudaMemcpy(h_output_arr, output_arr, output_size, cudaMemcpyDeviceToHost);
 		this->output = Eigen::MatrixXf::Map(h_output_arr, input.rows(), weights.cols());
-		// std::cout << "My kernel output: " << this->output << std::endl;
+		std::cout << "My kernel output: " << this->output << std::endl;
 		return this->output;
 	}
 
