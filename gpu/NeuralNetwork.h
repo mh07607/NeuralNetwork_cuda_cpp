c556dbe9177e89aad3ec2c2b485848438cb1f579
Arsal <mh07607@st.habib.edu.pk>
2024-03-10 17:02:00 +0500

    Update NeuralNetwork.h

--- gpu/NeuralNetwork.h
+++ gpu/NeuralNetwork.h
@@ -139,11 +139,11 @@ public:
 		
 		cudaDeviceSynchronize();
 		
-		cudaError_t cudaError = cudaGetLastError();
-		if(cudaError != cudaSuccess) {
-			printf("CUDA error after kernel launch: %s\n", cudaGetErrorString(cudaError));
-			//return 1; // return an error code
-		}
+		// cudaError_t cudaError = cudaGetLastError();
+		// if(cudaError != cudaSuccess) {
+		// 	printf("CUDA error after kernel launch: %s\n", cudaGetErrorString(cudaError));
+		// 	//return 1; // return an error code
+		// }
 
 		cudaMemcpy(weights.data(), d_weights, weights_size, cudaMemcpyDeviceToHost);
 		cudaMemcpy(bias.data(), d_bias, weights_size, cudaMemcpyDeviceToHost);
