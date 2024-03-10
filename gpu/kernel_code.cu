44e0b1e3dcdf1d10ec73c64978a2e9b44525f86d
Arsal <mh07607@st.habib.edu.pk>
2024-03-10 16:03:44 +0500

    debugging

--- gpu/kernel_code.cu
+++ gpu/kernel_code.cu
@@ -15,10 +15,6 @@ __global__ void DenseForwardPass(float* d_M, float* d_N, float* d_P, float * d_B
 	}
 }
 
-__global__ void test(){
-	int x = 5;
-}
-
 __global__ void transpose(float * d_N, float * d_M, int M, int N){
     int Row = blockIdx.y*blockDim.y+threadIdx.y;
 	// Calculate the column index of d_P and d_N
@@ -71,18 +67,18 @@ __global__ void DenseBackwardPass(
     float * d_weights_T = (float *) malloc (w_r * w_c * sizeof(float));
     float * d_input_T = (float *) malloc (i_r * i_c * sizeof(float));
 
-    dim3 block_size(32, 32, 1);
-    dim3 grid_size;
+    // dim3 block_size(32, 32, 1);
+    // dim3 grid_size;
 
-    grid_size.x = (w_r + block_size.x - 1) / block_size.x;
-    grid_size.y = (w_c + block_size.y - 1) / block_size.y;
-    transpose<<<grid_size, block_size>>>(d_weights_T, d_weights, w_r, w_c);
+    // grid_size.x = (w_r + block_size.x - 1) / block_size.x;
+    // grid_size.y = (w_c + block_size.y - 1) / block_size.y;
+    // transpose<<<grid_size, block_size>>>(d_weights_T, d_weights, w_r, w_c);
 
-    grid_size.x = (i_r + block_size.x - 1) / block_size.x;
-    grid_size.y = (i_c + block_size.y - 1) / block_size.y;
-    transpose<<<grid_size, block_size>>>(d_input_T, d_input, i_r, i_c);
+    // grid_size.x = (i_r + block_size.x - 1) / block_size.x;
+    // grid_size.y = (i_c + block_size.y - 1) / block_size.y;
+    // transpose<<<grid_size, block_size>>>(d_input_T, d_input, i_r, i_c);
 
-    __syncthreads();
+    // __syncthreads();
 
     // grid_size.x = (o_r + block_size.x - 1) / block_size.x;
     // grid_size.y = (w_r + block_size.y - 1) / block_size.y;
--- gpu/kernel_code.h
+++ gpu/kernel_code.h
@@ -4,7 +4,7 @@
 
 
 __global__ void DenseForwardPass(float* d_M, float* d_N, float* d_P, float * d_B, int M, int N, int P);
-__global__ void test();
+// __global__ void test();
 __global__ void DenseBackwardPass();
 __global__ void MatrixMulKernel(float* d_M, float* d_N, float* d_P, float * d_B, int M, int N, int P);
 __global__ void MatrixSubtractionKernel(float* d_M, float* d_N, int lr, int M, int N);
