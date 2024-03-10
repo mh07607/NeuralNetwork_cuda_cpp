a3d00862831aa7fe1487a00b01cf4439af708cd2
Arsal <mh07607@st.habib.edu.pk>
2024-03-10 15:43:38 +0500

    Update kernel_code.h

--- gpu/kernel_code.h
+++ gpu/kernel_code.h
@@ -7,6 +7,7 @@ __global__ void DenseForwardPass(float* d_M, float* d_N, float* d_P, float * d_B
 __global__ void test();
 __global__ void DenseBackwardPass();
 __global__ void MatrixMulKernel(float* d_M, float* d_N, float* d_P, float * d_B, int M, int N, int P);
+__global__ void MatrixSubtractionKernel(float* d_M, float* d_N, int lr, int M, int N);
 __global__ void DenseBackwardPass(
     float * d_outputError,
     float * d_input,
