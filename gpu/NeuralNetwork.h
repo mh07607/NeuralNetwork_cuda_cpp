1db3f921b79e1b2018cd20a6d15acbe941eb3e45
Arsal <mh07607@st.habib.edu.pk>
2024-03-10 16:33:41 +0500

    Update NeuralNetwork.h

--- gpu/NeuralNetwork.h
+++ gpu/NeuralNetwork.h
@@ -97,6 +97,13 @@ public:
 		// For retrieving the input error on host
 		float * h_inputError = (float *) malloc (inputError_size);
 
+		cudaError_t cudaError = cudaGetLastError();
+		if(cudaError != cudaSuccess) {
+			printf("CUDA error after kernel launch: %s\n", cudaGetErrorString(cudaError));
+			//return 1; // return an error code
+		}
+
+
 		cudaMalloc((void **) &d_outputError, outputError_size);
 		cudaMalloc((void **) &d_input, input_size);
 		cudaMalloc((void **) &d_weights, weights_size);
@@ -110,12 +117,7 @@ public:
 		cudaMemcpy(d_weights, weights.data(), weights_size, cudaMemcpyHostToDevice);
 		cudaMemcpy(d_outputError, outputError.data(), outputError_size, cudaMemcpyHostToDevice);
 
-		cudaError_t cudaError = cudaGetLastError();
-		if(cudaError != cudaSuccess) {
-			printf("CUDA error after kernel launch: %s\n", cudaGetErrorString(cudaError));
-			//return 1; // return an error code
-		}
-
+		
 		DenseBackwardPass<<<1, 1>>>
 		(d_outputError,
 	     d_input,
